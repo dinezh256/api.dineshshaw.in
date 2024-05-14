18d2c8cf90d2b03affe7ed9d3a0ef4a4860d90b9
Dinesh Shaw <idineshshaw7@gmail.com>
2024-05-14 13:44:54 +0530

    Backend landing page

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ dbConnection();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (_, res) => {
   res.sendFile(__dirname + "/index.html");
