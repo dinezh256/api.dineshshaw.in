12ae48206addc146e7a01964f57001f97a0c86bb
Dinesh Shaw <idineshshaw7@gmail.com>
2024-05-15 15:53:48 +0530

    cors fixes

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+// const userRoutes = require("./routes/users");
+// const authRoutes = require("./routes/auth");
+const viewRoutes = require("./routes/view");
+
+const dbConnection = require("./database");
+const { isDevelopment } = require("./utils");
 
 const whitelist = ['https://api.dineshshaw.in', 'https://dineshshaw.in', 'https://www.dineshshaw.in']
 
 const corsOptions = {
   origin: function (origin, callback) {
     console.log(origin)
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (whitelist.indexOf(origin) !== -1 || isDevelopment ? true : !origin) {
       callback(null, true);
     } else {
       callback(new Error("Request from unauthorized origin"));
@@ -16,11 +22,7 @@ const corsOptions = {
   },
 };
 
-// const userRoutes = require("./routes/users");
-// const authRoutes = require("./routes/auth");
-const viewRoutes = require("./routes/view");
 
-const dbConnection = require("./database");
 
 const app = express();
 dbConnection();
@@ -28,8 +30,6 @@ dbConnection();
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/public'));
-
-app.options('*', cors(corsOptions))
 
 app.get("/", (_, res) => {
   res.sendFile(__dirname + "/index.html");
